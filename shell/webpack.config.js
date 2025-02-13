4392f15671bbb3d235e7b5c8fa9ca2f02fe989ee
Pito <90179414+vpovedamaral@users.noreply.github.com>
2025-02-13 09:25:19 +0100

    Création du fichier webpack pour le shell

---
+++ shell/webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    devServer: {
+        port: 3000,
+        historyApiFallback: true,
+    },
+    output: {
+        publicPath: 'http://localhost:3000/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: { presets: ['@babel/preset-react'] },
+            },
+        ],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'shell',
+            remotes: {
+                header: 'header@http://localhost:3001/remoteEntry.js',
+            },
+            shared: {
+                react: { singleton: true, requiredVersion: '^17.0.2' },
+                'react-dom': { singleton: true, requiredVersion: '^17.0.2' },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
