2746406a9df8a898e4ad2fcbf0415f596b1450e7
Pito <90179414+vpovedamaral@users.noreply.github.com>
2025-02-13 09:25:09 +0100

    Création du fichier webpack pour le header

---
+++ header/webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    devServer: {
+        port: 3001,
+        historyApiFallback: true,
+    },
+    output: {
+        publicPath: 'http://localhost:3001/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: { presets: ['@babel/preset-react'] },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'header',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './Header': './src/Header.jsx',
+            },
+            shared: {
+                react: { singleton: true, requiredVersion: '^17.0.2' },
+                'react-dom': { singleton: true, requiredVersion: '^17.0.2' },
+            },
+        }),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+};
